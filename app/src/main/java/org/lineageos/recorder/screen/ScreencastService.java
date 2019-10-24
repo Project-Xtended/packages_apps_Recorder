LineageOS/android_packages_apps_Recorder

Recorder: friendship with storage ended, MediaProvider is my new best friend
Signed-off-by: Joey <joey@lineageos.org>
Change-Id: I73fb54386f7d26b9662f0c95a201b49ebfdc4944

app/src/main/java/org/lineageos/recorder/screen/ScreencastService.java
@@ -40,10 +40,12 @@ import android.util.Log;
 import android.view.Surface;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 
 import org.lineageos.recorder.R;
 import org.lineageos.recorder.RecorderActivity;
+import org.lineageos.recorder.utils.MediaProviderHelper;
 import org.lineageos.recorder.utils.LastRecordHelper;
 import org.lineageos.recorder.utils.Utils;
 
@@ -55,7 +57,7 @@ import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class ScreencastService extends Service {
+public class ScreencastService extends Service implements MediaProviderHelper.OnContentWritten {
     private static final String LOGTAG = "ScreencastService";
 
     private static final String SCREENCAST_NOTIFICATION_CHANNEL =
@@ -190,6 +192,14 @@ public class ScreencastService extends Service {
         super.onDestroy();
     }
 
+    @Override
+    public void onContentWritten(@Nullable String uri) {
+        stopForeground(true);
+        if (uri != null) {
+            sendShareNotification(uri);
+        }
+    }
+
     private int startScreencasting(Intent intent) {
         if (hasNoAvailableSpace()) {
             Toast.makeText(this, R.string.screen_insufficient_storage,
@@ -299,10 +309,8 @@ public class ScreencastService extends Service {
             mTimer.cancel();
             mTimer = null;
         }
-        stopForeground(true);
-        if (mPath != null) {
-            sendShareNotification(mPath.getPath());
-        }
+
+        MediaProviderHelper.addVideoToContentProvider(getContentResolver(), mPath, this);
     }
 
     private void stopCasting() {
@@ -334,24 +342,24 @@ public class ScreencastService extends Service {
         mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
     }
 
-    private NotificationCompat.Builder createShareNotificationBuilder(String file) {
+    private NotificationCompat.Builder createShareNotificationBuilder(String uri) {
         Intent intent = new Intent(this, RecorderActivity.class);
         PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
 
         PendingIntent playPIntent = PendingIntent.getActivity(this, 0,
-                LastRecordHelper.getOpenIntent(this, file, "video/mp4"),
+                LastRecordHelper.getOpenIntent(uri, "video/mp4"),
                 PendingIntent.FLAG_CANCEL_CURRENT);
         PendingIntent sharePIntent = PendingIntent.getActivity(this, 0,
-                LastRecordHelper.getShareIntent(this, file, "video/mp4"),
+                LastRecordHelper.getShareIntent(uri, "video/mp4"),
                 PendingIntent.FLAG_CANCEL_CURRENT);
         PendingIntent deletePIntent = PendingIntent.getActivity(this, 0,
                 LastRecordHelper.getDeleteIntent(this, false),
                 PendingIntent.FLAG_CANCEL_CURRENT);
 
         long timeElapsed = SystemClock.elapsedRealtime() - mStartTime;
-        LastRecordHelper.setLastItem(this, file, timeElapsed, false);
+        LastRecordHelper.setLastItem(this, uri, timeElapsed, false);
 
-        Log.i(LOGTAG, "Video complete: " + file);
+        Log.i(LOGTAG, "Video complete: " + uri);
 
         return new NotificationCompat.Builder(this, SCREENCAST_NOTIFICATION_CHANNEL)
                 .setWhen(System.currentTimeMillis())

app/src/main/java/org/lineageos/recorder/sounds/SoundRecorderService.java
@@ -30,6 +30,8 @@ import android.media.MediaRecorder;
 import android.os.Build;
 import android.os.Environment;
 import android.os.IBinder;
+
+import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 import android.text.format.DateUtils;
@@ -38,6 +40,7 @@ import android.util.Log;
 import org.lineageos.recorder.R;
 import org.lineageos.recorder.RecorderActivity;
 import org.lineageos.recorder.utils.LastRecordHelper;
+import org.lineageos.recorder.utils.MediaProviderHelper;
 import org.lineageos.recorder.utils.Utils;
 
 import java.io.BufferedOutputStream;
@@ -50,7 +53,7 @@ import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class SoundRecorderService extends Service {
+public class SoundRecorderService extends Service implements MediaProviderHelper.OnContentWritten {
 
     static final String EXTENSION = ".pcm";
     private static final String ACTION_STARTED = "org.lineageos.recorder.sounds.STARTED_SOUND";
@@ -134,6 +137,17 @@ public class SoundRecorderService extends Service {
         super.onDestroy();
     }
 
+    @Override
+    public void onContentWritten(@Nullable String uri) {
+        mStatus = RecorderStatus.STOPPED;
+        mOutFilePath = uri;
+        Intent intent = new Intent(ACTION_STOPPED);
+        intent.putExtra(EXTRA_FILE, mOutFilePath);
+        sendBroadcast(intent);
+        createShareNotification();
+        stopForeground(true);
+    }
+
     public boolean isRecording() {
         return mStatus == RecorderStatus.RECORDING;
     }
@@ -201,11 +215,8 @@ public class SoundRecorderService extends Service {
             oldFile.delete();
         }
 
-        mStatus = RecorderStatus.STOPPED;
-        Intent intent = new Intent(ACTION_STOPPED);
-        intent.putExtra(EXTRA_FILE, mOutFilePath);
-        sendBroadcast(intent);
-        stopForeground(true);
+        MediaProviderHelper.addSoundToContentProvider(
+                getContentResolver(), new File(mOutFilePath), this);
     }
 
     private File createNewAudioFile() {
@@ -311,10 +322,10 @@ public class SoundRecorderService extends Service {
         PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
 
         PendingIntent playPIntent = PendingIntent.getActivity(this, 0,
-                LastRecordHelper.getOpenIntent(this, mOutFilePath, "audio/wav"),
+                LastRecordHelper.getOpenIntent(mOutFilePath, "audio/wav"),
                 PendingIntent.FLAG_CANCEL_CURRENT);
         PendingIntent sharePIntent = PendingIntent.getActivity(this, 0,
-                LastRecordHelper.getShareIntent(this, mOutFilePath, "audio/wav"),
+                LastRecordHelper.getShareIntent(mOutFilePath, "audio/wav"),
                 PendingIntent.FLAG_CANCEL_CURRENT);
         PendingIntent deletePIntent = PendingIntent.getActivity(this, 0,
                 LastRecordHelper.getDeleteIntent(this, true),

Project-Xtended/packages_apps_Recorder

Signed-off-by: DennySPB <dennyspb@gmail.com>

app/src/main/java/org/lineageos/recorder/screen/ScreencastService.java
@@ -26,6 +26,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.media.AudioSystem;
+import android.net.Uri;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.IBinder;
@@ -34,19 +35,23 @@ import android.telephony.TelephonyManager;
 import android.text.format.DateUtils;
 import android.util.Log;
 
+import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import org.lineageos.recorder.R;
 import org.lineageos.recorder.RecorderActivity;
 import org.lineageos.recorder.utils.LastRecordHelper;
+import org.lineageos.recorder.utils.MediaProviderHelper;
 import org.lineageos.recorder.utils.PreferenceUtils;
 import org.lineageos.recorder.utils.Utils;
 
+import java.io.File;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class ScreencastService extends Service implements ScreenRecorder.ScreenRecorderResultCallback {
+public class ScreencastService extends Service implements ScreenRecorder.ScreenRecorderResultCallback,
+                                                             MediaProviderHelper.OnContentWritten {
 
     private static final String SCREENCAST_NOTIFICATION_CHANNEL =
             "screencast_notification_channel";
@@ -64,6 +69,7 @@ public class ScreencastService extends Service implements ScreenRecorder.ScreenR
     private NotificationManager mNotificationManager;
     private boolean mAlreadyNotified = false;
     private final BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+
         @Override
         public void onReceive(Context context, Intent intent) {
             final String action = intent.getAction();
@@ -278,11 +284,19 @@ public class ScreencastService extends Service implements ScreenRecorder.ScreenR
         stopRecording(false);
     }
 
+
+    @Override
+    public void onContentWritten(@Nullable String uri) {
+        stopForeground(true);
+        if (uri != null) {
+            sendShareNotification(uri);
+        }
+    }
+
     private void stopRecording(boolean fromOnDestroy) {
         Utils.stopOverlayService(this);
         Utils.setStatus(Utils.PREF_RECORDING_NOTHING, this);
         mHandler.removeCallbacksAndMessages(null);
-
         String recorderPath = null;
         if (mRecorder != null) {
             mRecorder.setStopping(true);
@@ -291,9 +305,10 @@ public class ScreencastService extends Service implements ScreenRecorder.ScreenR
             mRecorder = null;
         }
         stopTimer();
-        stopForeground(true);
-        if (!fromOnDestroy && recorderPath != null) {
-            sendShareNotification(recorderPath);
+            Log.e(LOGTAG, "Path: " +recorderPath);
+
+        if (!fromOnDestroy) {
+            MediaProviderHelper.addVideoToContentProvider(getContentResolver(), new File(recorderPath), this);
         } else if (!fromOnDestroy && !mAlreadyNotified) {
             Utils.notifyError(getString(R.string.unknow_error), true, ScreencastService.this, mNotificationManager, false);
         }
@@ -324,20 +339,21 @@ public class ScreencastService extends Service implements ScreenRecorder.ScreenR
         mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
     }
 
-    private NotificationCompat.Builder createShareNotificationBuilder(String file) {
+    private NotificationCompat.Builder createShareNotificationBuilder(String uriStr) {
+        Uri uri = Uri.parse(uriStr);
         PendingIntent playPIntent = PendingIntent.getActivity(this, 0,
-                LastRecordHelper.getOpenIntent(this, file, "video/mp4"),
+                LastRecordHelper.getOpenIntent(uri, "video/mp4"),
                 PendingIntent.FLAG_CANCEL_CURRENT);
         PendingIntent sharePIntent = PendingIntent.getActivity(this, 0,
-                LastRecordHelper.getShareIntent(this, file, "video/mp4"),
+                LastRecordHelper.getShareIntent(uri, "video/mp4"),
                 PendingIntent.FLAG_CANCEL_CURRENT);
         PendingIntent deletePIntent = PendingIntent.getActivity(this, 0,
                 LastRecordHelper.getDeleteIntent(this, false),
                 PendingIntent.FLAG_CANCEL_CURRENT);
 
-        LastRecordHelper.setLastItem(this, file, sElapsedTimeInSeconds, false);
+        Log.e(LOGTAG, "Uri: " +uriStr);
 
-        Log.i(LOGTAG, "Video complete: " + file);
+        LastRecordHelper.setLastItem(this, uriStr, sElapsedTimeInSeconds, false);
 
         return new NotificationCompat.Builder(this, Utils.RECORDING_DONE_NOTIFICATION_CHANNEL)
                 .setWhen(System.currentTimeMillis())

app/src/main/java/org/lineageos/recorder/sounds/SoundRecorderService.java
@@ -27,11 +27,13 @@ import android.content.IntentFilter;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Environment;
 import android.os.IBinder;
 import android.text.format.DateUtils;
 import android.util.Log;
 
+import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
@@ -39,6 +41,7 @@ import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import org.lineageos.recorder.R;
 import org.lineageos.recorder.RecorderActivity;
 import org.lineageos.recorder.utils.LastRecordHelper;
+import org.lineageos.recorder.utils.MediaProviderHelper;
 import org.lineageos.recorder.utils.Utils;
 
 import java.io.BufferedOutputStream;
@@ -51,7 +54,7 @@ import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class SoundRecorderService extends Service {
+public class SoundRecorderService extends Service implements MediaProviderHelper.OnContentWritten {
 
     static final String EXTENSION = ".pcm";
     private static final String ACTION_STARTED = "org.lineageos.recorder.sounds.STARTED_SOUND";
@@ -138,6 +141,17 @@ public class SoundRecorderService extends Service {
         super.onDestroy();
     }
 
+    @Override
+    public void onContentWritten(@Nullable String uri) {
+        mStatus = RecorderStatus.STOPPED;
+        mOutFilePath = uri;
+        Intent intent = new Intent(ACTION_STOPPED);
+        intent.putExtra(EXTRA_FILE, mOutFilePath);
+        sendBroadcast(intent);
+        createShareNotification();
+        stopForeground(true);
+    }
+
     public boolean isRecording() {
         return mStatus == RecorderStatus.RECORDING;
     }
@@ -206,11 +220,8 @@ public class SoundRecorderService extends Service {
             oldFile.delete();
         }
 
-        mStatus = RecorderStatus.STOPPED;
-        Intent intent = new Intent(ACTION_STOPPED);
-        intent.putExtra(EXTRA_FILE, mOutFilePath);
-        sendBroadcast(intent);
-        stopForeground(true);
+        MediaProviderHelper.addSoundToContentProvider(
+                getContentResolver(), new File(mOutFilePath), this);
     }
 
     private File createNewAudioFile() {
@@ -313,11 +324,12 @@ public class SoundRecorderService extends Service {
     }
 
     public void createShareNotification() {
+        Uri outFileUri = Uri.parse(mOutFilePath);
         PendingIntent playPIntent = PendingIntent.getActivity(this, 0,
-                LastRecordHelper.getOpenIntent(this, mOutFilePath, "audio/wav"),
+                LastRecordHelper.getOpenIntent(outFileUri, "audio/wav"),
                 PendingIntent.FLAG_CANCEL_CURRENT);
         PendingIntent sharePIntent = PendingIntent.getActivity(this, 0,
-                LastRecordHelper.getShareIntent(this, mOutFilePath, "audio/wav"),
+                LastRecordHelper.getShareIntent(outFileUri, "audio/wav"),
                 PendingIntent.FLAG_CANCEL_CURRENT);
         PendingIntent deletePIntent = PendingIntent.getActivity(this, 0,
                 LastRecordHelper.getDeleteIntent(this, true),